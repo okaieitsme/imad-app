3ca954f00bc85d6aa34b18719b12bcf7482477d5
okaieitsme <okaieitsme@gmail.com>
2018-02-18 16:03:04 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,11 @@
+//code for counter button
+var button = document.getElementById('counter');
+var counter = 0;
+
+button.onclick = function() {
+    
+    counter = counter +1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
+
