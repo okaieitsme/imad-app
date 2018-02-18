42020074b432727cdf316c30994ea3fde43f6f6a
okaieitsme <okaieitsme@gmail.com>
2018-02-18 11:57:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,15 @@
 console.log('Loaded!');
 
+
+// Change text of main-div
 var element = document.getElementById("main-text");
 
 element.innerHTML = "New value";
+
+//move image
+
+var img = document.getElementById("madi");
+
+img.onClick = function() {
+    img.style.marginLeft = '100px';
+};
