9df71e999e7b831ab969e9103fa9f70796f8fec0
okaieitsme <okaieitsme@gmail.com>
2018-02-18 12:12:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -12,9 +12,9 @@ var img = document.getElementById("madi");
 var marginLeft = 0;
 function moveRight () {
     marginLeft = marginLeft + 10;
-    img.style.marginLeft = margingLeft + 'px';
+    img.style.marginLeft = marginLeft + 'px';
 }
 
 img.onclick = function() {
-    var interval = setInterval(moveRight, 200);
+    var interval = setInterval(moveRight, 100);
 };
