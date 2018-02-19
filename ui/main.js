6b2517a66c293fc1a8f60aa8f059948cd4312da1
okaieitsme <okaieitsme@gmail.com>
2018-02-19 20:58:31 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -27,8 +27,8 @@ submit.onclick = function() {
     var names = ['name1', 'name2', 'name3', 'name4' ];
     var lsit = '';
     for(var i=0; i<names.length; i++) {
-        list += '<li>' +names[i] + '</li>';
+        list += '<li>' + names[i] + '</li>';
     }
     var ul = document.getElementById('namelist');
     ul.innerHTML = list;
-}
+};
