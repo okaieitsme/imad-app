ae98a6d5c0baa060efd6d7219290f9e987d34307
okaieitsme <okaieitsme@gmail.com>
2018-02-19 21:15:36 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,6 +93,7 @@ app.get('/counter', function (req, res) {
 var names = [];
 app.get('/submit-name', function (req, res) {
     var name = req.query.name;
+    send(name);
     names.push(name);
     res.send(JSON.stringify(names));
 });
