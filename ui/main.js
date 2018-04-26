4e9528acb97e0c77792f2627fac76d9926051019
okaieitsme <okaieitsme@gmail.com>
2018-04-26 11:43:55 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,21 +3,21 @@ var button = document.getElementById('counter');
 
 button.onclick = function() {
     
-    var request = new XMLHttpRequest();
-    
-    request.onreadystatechange = function() {
-        if(request.readyState === XMLHttpRequest.DONE) {
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
+            var request = new XMLHttpRequest();
+            
+            request.onreadystatechange = function() {
+                if(request.readyState === XMLHttpRequest.DONE) {
+                    if(request.status === 200) {
+                        var counter = request.responseText;
+                        var span = document.getElementById('count');
+                        span.innerHTML = counter.toString();
+                        
+                    }
+                }
                 
-            }
-        }
-        
-    };
-    request.open('GET','http://okaieitsme.imad.hasura-app.io/counter', true);
-    request.send(null);
+            };
+            request.open('GET','http://okaieitsme.imad.hasura-app.io/counter', true);
+            request.send(null);
 };
 
 var submit = document.getElementById('submit_btn');
